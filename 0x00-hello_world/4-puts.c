29cd4fe1e820178309e4c906b8e4fb97c07a46ac
Jilroge7 <1672@holbertonschool.com>
2020-02-06 05:38:43 +0000

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+* main Entry point
+*
+* Script to print quoted statement to output.
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char string[51];
+
+	strcpy(string, "Programming is like building a multilingual puzzle\n");
+
+	puts(string);
+
+	return (0);
+}
