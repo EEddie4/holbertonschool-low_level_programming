31a42241ae503ac070e774369650d8e8d26d00d0
Jilroge7 <1672@holbertonschool.com>
2020-02-07 19:14:30 +0000

    new iteration

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,20 +13,18 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-		printf("Last digit of n is %d", v);
+		printf("Last digit of %d  is %d", n, v);
 	if (v > 5)
 	{
 		printf("and is greater than 5\n");
 	}
 	else
-		if (v == 0)
-	{
-		printf("and is 0\n");
-	}
-	else
-	(v != 0 && v < 6)
+		if (v != 0 && v < 6)
 	{
 		printf("and is less than 6 and not 0\n");
 	}
+	else
+		printf("and is 0\n");
+
 	return (0);
 }
