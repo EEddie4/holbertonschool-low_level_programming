8d176b34bafe542a9e4af19f0f5154543aaf4aee
Jilroge7 <1672@holbertonschool.com>
2020-02-07 00:50:23 +0000

    Adding last_digit program sc

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,31 @@
+#include<stdlib.h>
+#include<time.h>
+#include<stdio.h>
+/**
+* main - Entry point
+* if...else if... else statement to determine integers and value
+* Return: Always 0 (Sucess)
+*/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+		printf("Last digit of %d is %d", n, n % 10);
+	if (n > 5)
+	{
+		printf("and is greater than 5\n");
+	}
+	else
+		if (n == 0)
+	{
+		printf("and is 0\n");
+	}
+	else
+	(n != 0 && n < 6)
+	{
+		printf("and is less than 6 and not 0\n");
+	}
+	return (0);
+}
