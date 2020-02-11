9a96615ffc909ef5accf610c9f1b858ba8fa228c
Jilroge7 <1672@holbertonschool.com>
2020-02-11 16:51:13 +0000

    Update to print 10x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,17 +1,22 @@
-include"holberton.h"
+#include"holberton.h"
 /**
+* print_alphabet_x10 - print alphabet 10x
+*
 * description: print alphabet lowercase in ascending order 10x
 * Return: 0
 */
 void print_alphabet_x10(void)
 {
 	char ch = 'a';
+	int i = 0;
 
-	while (ch <= 'z')
+	while (i < 10)
 	{
-		_putchar(ch);
-		ch++;
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+		i++;
+		_putchar(10);
 	}
-	putchar(10);
-	return (0);
 }
