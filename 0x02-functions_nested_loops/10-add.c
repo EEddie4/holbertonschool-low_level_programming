84a0018084278249a856e8f28a4baec1eaeeda19
Jilroge7 <1672@holbertonschool.com>
2020-02-12 02:37:25 +0000

    Adding placeholders for 9-11

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+* add - function
+*
+* Description: function that adds two integers and returns the result.
+* Return: Always 0 (success)
+*/
+int add(int, int)
+{
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+* print_to_98 - function
+* @n: first operand
+*
+* description: prints all natural numbers from n to 98, followed by a new line
+* Return: Always 0 (success)
+*/
+void print_to_98(int n)
+{
+}
