f2a5e67a84c1468c123d986c8185c694f993f087
Jilroge7 <1672@holbertonschool.com>
2020-02-14 21:01:27 +0000

    Edit to fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,21 +15,23 @@ int main(void)
 		{
 			if (i % 5 == 0)
 			{
-				printf("FizzBuzz ");
+				printf("FizzBuzz");
 			}
 			else
 			{
-				printf("Fizz ");
+				printf("Fizz");
 			}
 		}
 		else if (i % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else
 		{
-			printf("%d ", i);
+			printf("%d", i);
 		}
+		if (i < 100)
+			putchar(' ');
 	}
 	printf("\n");
 	return (0);
