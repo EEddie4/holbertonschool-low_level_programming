1e3dd261b459044c09404ec2e020cd7af07e1062
Jilroge7 <1672@holbertonschool.com>
2020-02-18 16:32:56 +0000

    Adding swap_int

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include "holberton.h"
-/*
+/**
 * swap_int - function
 * @a: first operand
 * @b: second operand
 *
-* Description: swap values of two integers
+* Description:swap_int values of two integers
 * Return: Always 0
 */
 void swap_int(int *a, int *b)
