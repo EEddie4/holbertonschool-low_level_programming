c2960e8ac6c6f7c61099490abe314916ee266f14
Jilroge7 <1672@holbertonschool.com>
2020-02-19 23:08:57 +0000

    Adding task 8 print array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdio.h>
 /**
 * print_array - function
 * @a: operand
@@ -9,4 +10,15 @@
 */
 void print_array(int *a, int n)
 {
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+	if (i != 0)
+	{
+		printf(", ");
+	}
+		printf("%d", a[i]);
+	}
+	printf("\n");
 }
