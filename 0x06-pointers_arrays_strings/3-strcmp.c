7980967a6ab183c563a27996b8facbd141ca2ff2
Jilroge7 <1672@holbertonschool.com>
2020-02-20 21:43:29 +0000

    adding update to 3-strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,10 +11,11 @@ int _strcmp(char *s1, char *s2)
 {
 	int len;
 
-	for (len = 0; s1[len] && s2[len]; len++)
+	for (len = 0; s1[len] && s2[len];)
 	{
 		if (s1[len] == s2[len])
 			len++;
+		else
 			break;
 	}
 	return (s1[len] - s2[len]);
