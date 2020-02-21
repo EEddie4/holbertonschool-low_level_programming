3daf8f5a54f5e06ea286f568e484821c9b3658d8
Jilroge7 <1672@holbertonschool.com>
2020-02-21 04:02:33 +0000

    adding placeholder for 7-leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+* leet - function
+* @s: array
+*
+* Description: swap letters with integers
+* Return: s
+*/
+char *leet(char *s)
+{
+	int i;
+
+	for (i = 0; s[i]; i++)
+		;
+
+	return (s);
+}
