041efd52632e26148521722a336aa73bdbb9bad9
Jilroge7 <1672@holbertonschool.com>
2020-02-21 04:12:19 +0000

    adding placeholder for rot13

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+* rot13 - function
+* @s: operand
+*
+* Description: Encodes a string using rot13
+* Return: s
+*/
+char *rot13(char *s)
+{
+	int i;
+
+	for (i = 0; s[i]; i++)
+		;
+
+	return (s);
+}
