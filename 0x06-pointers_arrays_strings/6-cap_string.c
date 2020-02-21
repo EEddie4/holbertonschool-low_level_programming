a626609b2667cca6853d58a9dae80f8b96603ac6
Jilroge7 <1672@holbertonschool.com>
2020-02-21 03:51:16 +0000

    adding placeholder for 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+* cap_string - function
+* @s: pointer
+*
+*Description: function to capitalize all words of a string
+* Return: char
+*/
+char *cap_string(char *s)
+{
+	int i;
+
+	for (i = 0; s[i]; i++)
+		;
+
+	return (s);
+}
