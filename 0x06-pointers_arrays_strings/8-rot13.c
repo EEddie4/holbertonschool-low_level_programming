bd460a2eccf88e2018f748a255c8813cced0cc3f
Jilroge7 <1672@holbertonschool.com>
2020-02-22 00:10:56 +0000

    Editto cap

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -1,17 +1,27 @@
 #include "holberton.h"
 /**
 * rot13 - function
+* @alpha: alphabet string
+* @cipher: rot13 cipher
 * @s: operand
 *
 * Description: Encodes a string using rot13
 * Return: s
 */
-char *rot13(char *s)
+char *rot13(char *alpha, char *cipher, char *s)
 {
-	int i;
+	int i, j;
+
+	alpha[53];
+	cipher[53];
 
 	for (i = 0; s[i]; i++)
-		;
-
-	return (s);
+	{
+		for (j = 0; alpha[j]; j++)
+		if (s[i] == alpha[j])
+		{
+			s[i] = cipher[j];
+		}
+		return (s);
+	}
 }
