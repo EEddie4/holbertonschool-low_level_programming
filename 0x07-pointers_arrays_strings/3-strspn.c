dc9c19623d5399f77cc41f4712841fbe1899ff29
Jilroge7 <1672@holbertonschool.com>
2020-02-24 23:18:28 +0000

    strchr function and header update

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+* _strspn - function
+* @s: return num bytes of
+* @accept: bytes from
+*
+* Description: function to get through length of prefix substring
+* Return: s
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+
