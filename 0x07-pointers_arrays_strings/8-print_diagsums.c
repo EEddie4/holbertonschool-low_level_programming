5443e834b0e760fb978a8bedc07f48d6edc54e73
Jilroge7 <1672@holbertonschool.com>
2020-02-26 00:27:29 +0000

    edit to src print diagsums

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,5 +1,5 @@
 #include "holberton.h"
-#define NULL 0
+#include<stdio.h>
 /**
 * print_diagsums - function
 * @a: first operand
@@ -10,6 +10,27 @@
 */
 void print_diagsums(int *a, int size)
 {
-	int i;
+	int r, c, sum, sum1, loc;
 
+	sum = 0;
+	sum1 = 0;
+
+	for (r = 0; r <= (size - 1); r++)
+	{
+		for (c = 0; c <= (size - 1); c++)
+		{
+			if (r == c)
+			{
+				loc = ((size * r) + c);
+				sum = *(a + loc) + sum;
+			}
+			if (c == (size - 1) - r)
+			{
+				loc = ((size * r) + c);
+				sum1 = *(a + loc) + sum1;
+			}
+		}
+	}
+	printf("%d", sum);
+	printf(", %d\n", sum1);
 }
