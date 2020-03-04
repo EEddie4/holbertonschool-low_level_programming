019003d38442f1bf37f25c24a971a01e967f8c85
Jilroge7 <1672@holbertonschool.com>
2020-03-04 18:02:39 +0000

    edit to src strconcat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,7 +17,7 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL || s2 == NULL)
 	{
-		return (NULL);
+		return (0);
 	}
 	combo = malloc((_strlen(s1) + _strlen(s2) + 1) * sizeof(char));
 	if (combo == NULL)
@@ -37,7 +37,7 @@ char *str_concat(char *s1, char *s2)
 		combo[i] = s2[k];
 		i++;
 	}
-	combo[i + 1] = '\0';
+	combo[i] = '\0';
 	return (combo);
 }
 
