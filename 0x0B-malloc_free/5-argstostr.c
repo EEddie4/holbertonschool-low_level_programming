883ae7cd10074cf178759801752121c4c515733f
Jilroge7 <1672@holbertonschool.com>
2020-03-04 06:07:21 +0000

    adding placeholder for argstostr

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+* argstostr - function
+* @ac: arg1
+* @av: arg2
+*
+* Description: concatenate all args of your program
+* Return: ptr to new str or null if fail
+*/
+char *argstostr(int ac, char **av)
+{
+	int i;
+
+	for (i = 0; i < ac; i++)
+		;
+
+	while (av)
+		;
+
+	return (0);
+}
