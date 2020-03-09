cb943841b500ec402bb7da868f9f77819e5f8781
Jilroge7 <1672@holbertonschool.com>
2020-03-09 23:38:59 +0000

    First commit to 0x0D struct, typedef with readme and task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+int _putchar (char c);
+/**
+ * struct dog - data structure
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Description: dog with name, age, and owner's name.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
