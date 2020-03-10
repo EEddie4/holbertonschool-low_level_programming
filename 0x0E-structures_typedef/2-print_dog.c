2964504ce6ca4f6c271e31de12dfbc7b699ceb5b
Jilroge7 <1672@holbertonschool.com>
2020-03-10 02:45:56 +0000

    update to remove return and add if condition to printdog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,6 +12,8 @@ void print_dog(struct dog *d)
 {
 	struct dog;
 
+	if (d == NULL)
+		return;
 	if (d->name == NULL)
 	{
 		printf("Name: (nil)\n");
@@ -20,11 +22,8 @@ void print_dog(struct dog *d)
 	{
 		printf("Owner: (nil)\n");
 	}
-	if (d != '\0')
-	{
-		printf("Name: %s\n", d->name);
-		printf("Age: %.1f\n", d->age);
-		printf("Owner: %s\n", d->owner);
-	}
-	return;
+
+	printf("Name: %s\n", d->name);
+	printf("Age: %.1f\n", d->age);
+	printf("Owner: %s\n", d->owner);
 }
