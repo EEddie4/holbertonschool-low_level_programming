bb2335cf9b7afbf852060ea85bc6ec4faa410584
Jilroge7 <1672@holbertonschool.com>
2020-03-10 03:50:34 +0000

    editto if statements agan in printdog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,12 +15,10 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 		return;
 	if (d->name == NULL)
-		printf("Name: (nil)\n");
-	else
-		printf("Name: %s\n", d->name);
-	printf("Age: %.1f\n", d->age);
+		d->name = "(nil)";
 	if (d->owner == NULL)
-		printf("Owner: (nil)\n");
-	else
+		d->owner = "(nil)";
+	printf("Name: %s\n", d->name);
+	printf("Age: %.1f\n", d->age);
 	printf("Owner: %s\n", d->owner);
 }
