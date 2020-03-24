3af204f6db6d0554daee450a4ae7c9df690fe24d
Jilroge7 <1672@holbertonschool.com>
2020-03-24 18:42:04 +0000

    edit to add head is null condition

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,6 +10,8 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp = *head; /* Will store head ->next*/
 
+	if (*head == NULL)
+		return;
 	while (*head != NULL)
 	{
 		temp = (*head)->next;
