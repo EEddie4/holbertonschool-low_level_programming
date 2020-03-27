f49eaf583d6114e7848b5a13e5afeb54c69be044
Jilroge7 <1672@holbertonschool.com>
2020-03-27 01:37:10 +0000

    Adding broken src for 1-prt binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+* print_binary - function
+* @n:number to convert
+*
+* Description: Function to print binary conv of number given
+* Return: converted number
+*/
+void print_binary(unsigned long int n)
+{
+	unsigned long int i = 0;
+
+	if (n == 0)
+		_putchar(n + '0');
+
+	while (n > 0)
+	{
+		if ((n & 1) == 0)
+			i += (i + 0);
+		else
+			i += (i + 1);
+		n = n >> 1;
+		_putchar(i + '0');
+	}
+}
