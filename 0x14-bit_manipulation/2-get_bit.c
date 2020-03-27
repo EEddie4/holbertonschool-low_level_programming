62246c855a9e927f11349cf1d5fe7444021e8fb9
Jilroge7 <1672@holbertonschool.com>
2020-03-27 03:39:15 +0000

    edit to include error return case

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -11,9 +11,8 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int val;
 
-	if (n == 0)
+	if (index > sizeof(unsigned long int) * 8)
 		return (-1);
-
 	val = (n >> index) & 1;
 
 	return (val);
