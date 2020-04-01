b7f96e6c8339edfaf98f33d3e45d7d29c79a2295
Jilroge7 <1672@holbertonschool.com>
2020-04-01 05:34:51 +0000

    adding placeholder for cp

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+* main - entry point!
+*
+* Description: prgrm to copy the contents of a file to another file
+* Return: Always 0
+*/
+int main(void)
+{
+/**
+*	const char *file_to, file_from[1024];
+*	int FD_VALUE;
+*
+*	FD_VALUE = open(file_to, O_RDWR | O_TRUNC, 0641);
+*
+*	if (file_from == NULL || FD_VALUE == -1)
+*	{
+*		dprintf(STDERR_FILENO, "Error: Can't read from file NAME_OF_THE_FILE\n");
+*		exit(98);
+*	}
+*/	return (0);
+}
