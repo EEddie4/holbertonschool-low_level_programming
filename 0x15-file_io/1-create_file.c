e0318cb17d2bb10b9f17250e7cd0358132e85e15
Jilroge7 <1672@holbertonschool.com>
2020-04-01 04:16:59 +0000

    edit to partial src for task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,10 +17,9 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	while (text_content != '\0')
-	{
+	while (text_content[sz] != '\0')
 		sz++;
-	}
+	
 	wr = write(fd, text_content, sz);
 
 	if (wr == -1)
