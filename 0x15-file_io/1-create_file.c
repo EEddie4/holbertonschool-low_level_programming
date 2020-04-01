1ad8b66c6c6e0ce2598fe3673610375787122db3
Jilroge7 <1672@holbertonschool.com>
2020-04-01 04:48:49 +0000

    edit to src for task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,6 +20,9 @@ int create_file(const char *filename, char *text_content)
 	while (text_content[sz] != '\0')
 		sz++;
 
+	if (text_content == NULL)
+		text_content = "";
+
 	wr = write(fd, text_content, sz);
 
 	if (wr == -1)
