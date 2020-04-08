5a2127914ac9a49457348b32cdb49560f868b531
Jilroge7 <1672@holbertonschool.com>
2020-04-08 21:43:51 +0000

    adding src for cp3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,22 +1,86 @@
 #include "holberton.h"
+void cant_write(char *s);
+void cant_read(char *s);
 /**
 * main - entry point!
+* @s: string to read/writey
 *
-* Description: prgrm to copy the contents of a file to another file
+* Description: cp file_from file_to
 * Return: Always 0
 */
-int main(void)
+int main(int argc, char *argv[])
 {
+	char *buff[1024];
+	int fd1, fd2;
+	ssize_t rd, wr;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	if (argv[1] == NULL)
+	{
+		cant_read(argv[1]);
+		exit(98);
+	}
+	fd1 = open(argv[1], O_RDONLY);
+	if (fd1 == -1)
+		cant_read(argv[1]);
+
+	fd2 = open(argv[1], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd2 == -1)
+		cant_write(argv[2]);
+
+	rd = read(fd1, buff, 1024);
+	if (rd == -1)
+		cant_read(argv[1]);
+
+	wr = write(fd2, buff, rd);
+	if (wr == -1)
+		cant_write(argv[2]);
+	while (rd == 1024)
+	{
+		rd = read(fd1, buff, 1024);
+		if (rd == -1)
+			cant_read(argv[1]);
+		wr = write(fd2, buff, rd);
+		if (wr == 1)
+			cant_write(argv[2]);
+	}
+	if (close(fd1) < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1);
+		exit(100);
+	}
+	if (close(fd2) < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2);
+		exit(100);
+	}
+	return (0);
+}
 /**
-*	const char *file_to, file_from[1024];
-*	int FD_VALUE;
+* cant_read - function
+* @s: string to read
 *
-*	FD_VALUE = open(file_to, O_RDWR | O_TRUNC, 0641);
+* Description: error function if unable to read
+* Return: 0
+*/
+void cant_read(char *s)
+{
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", s);
+	exit(98);
+}
+/**
+* cant_write - function
+* @s: string to write
 *
-*	if (file_from == NULL || FD_VALUE == -1)
-*	{
-*		dprintf(STDERR_FILENO, "Error: Can't read from file NAME_OF_THE_FILE\n");
-*		exit(98);
-*	}
-*/	return (0);
+* Description: error func if unable to write
+* Return: 0
+*/
+void cant_write(char *s)
+{
+	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", s);
+	exit(99);
 }
