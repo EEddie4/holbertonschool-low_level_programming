2ac39cd97521b4557e46e39ec2c583cf8b86afde
Jilroge7 <1672@holbertonschool.com>
2020-05-04 00:48:54 +0000

    change to index condition

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -37,8 +37,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 	if (toindex == 0)
 	{
-		newNode->next = *h;
-		newNode->prev = NULL;
+		(*h)->prev = newNode;
 		*h = newNode;
 		return (newNode);
 	}
