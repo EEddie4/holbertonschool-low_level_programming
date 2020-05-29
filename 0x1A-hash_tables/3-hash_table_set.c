3fc7ec0493ed25d74b52788a345e3814cd6b140d
Jilroge7 <1672@holbertonschool.com>
2020-05-29 19:16:10 +0000

    adding task 3, fix to task 0

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,46 @@
+#include "hash_tables.h"
+/**
+* hash_table_set - function
+* @ht: table to add/update key/value to
+* @key: key. Cannot be empty string
+* @value: value associated with key
+*
+* Description: function to add an element to a hash table.
+* Return: 1 Success, 0 Fail
+*/
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *newNode, *tempNode;
+	unsigned long int index;
+
+	if (ht == NULL)
+		return (0);
+
+	index = (hash_djb2((const unsigned char *)key) % (ht->size));
+
+	if (key == NULL || value == NULL)
+		return (0);
+
+	if (ht->array[index] == NULL) /*slot is empty, put node here*/
+	{
+		newNode = malloc(sizeof(hash_node_t));
+		if (newNode == NULL)
+			return (0);
+		ht->array[index] = newNode;
+		newNode->key = (strdup(key));
+		newNode->value = (strdup(value));
+		newNode->next = NULL;
+		return (1);
+	}
+	tempNode = ht->array[index];
+	while (tempNode) /*while "head" is not NULL*/
+	{
+		if (strcmp(key, tempNode->key) == 0)
+		{/*checking when key matches the key we need, 0 is true*/
+			tempNode->value = (strdup(value));
+			return (1);
+		}
+		tempNode = tempNode->next;/*traversing*/
+	}
+	return (0);
+}
