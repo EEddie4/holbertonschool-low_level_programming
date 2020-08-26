c876a73ef28d676b95541557eb569d258f6ef467
Jilroge7 <1672@holbertonschool.com>
2020-08-26 19:59:47 +0000

    adding partial task 1, 2, and 3

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,38 @@
+#include "search_algos.h"
+/**
+* binary_search - function
+* @array: array to search
+* @size: size of array to search
+* @value:value to search for
+*
+* Description: Search for value in array using binary search algo
+* Return: Index of value, or -1 if value not present or array is NULL
+*/
+int binary_search(int *array, size_t size, int value)
+{
+	size_t index = 0, real_size = (size - 1);
+	size_t middle;
+
+	while (index <= real_size)
+	{
+		middle = ((index + real_size) / 2);
+		printf("Searching in array: %ld\n", index);
+		if (array[middle] < value)
+		{
+			index = (middle + 1);
+			if (index > real_size)
+				return (-1);
+		}
+		else if (array[middle] > value)
+		{
+			real_size = (middle - 1);
+			if (index > real_size)
+				return (-1);
+		}
+		else
+			return (middle);
+	}
+	return (-1);
+
+
+}
